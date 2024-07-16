f83b8376a1827ba87645853e5d6545f465599528
Alejodev <alejodev2022@gmail.com>
2024-07-15 20:48:54 -0500

    Conexion a la fake api

--- js/conexionApi.js
+++ js/conexionApi.js
@@ -1,8 +1,67 @@
-async function conectar(){
-    const conexion = await fetch('http://localhost:3000/productos');
+// Función para obtener la lista de productos
+async function listaProductos() {
+  try {
+    const conexion = await fetch("https://alurageek-fake-api-rose.vercel.app/product");
+    if (!conexion.ok) {
+      throw new Error('Error en la solicitud');
+    }
     const conexionConvertida = await conexion.json();
     console.log(conexionConvertida);
     return conexionConvertida;
+  } catch (error) {
+    alert("Error en la conexión: " + error.message);
+    console.error(error);
+  }
 }
 
-conectar();
+// Función para crear un nuevo producto
+async function nuevoProducto(nombre, precio, imagen, id) {
+  try {
+    const conexion = await fetch("https://alurageek-fake-api-rose.vercel.app/product", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        nombre: nombre,
+        precio: precio,
+        imagen: imagen,
+        id: id
+      })
+    });
+
+    if (!conexion.ok) {
+      throw new Error('Error en la solicitud');
+    }
+
+    const conexionConvertida = await conexion.json();
+    return conexionConvertida;
+  } catch (error) {
+    alert("Error en la conexión: " + error.message);
+    console.error(error);
+  }
+}
+
+// Función para eliminar un producto
+async function eliminarTarjeta(id) {
+  try {
+    const conexion = await fetch(`https://alurageek-fake-api-rose.vercel.app/product/${id}`, {
+      method: "DELETE",
+      headers: { "Content-type": "application/json" }
+    });
+
+    if (!conexion.ok) {
+      throw new Error('Error en la solicitud');
+    }
+
+    const conexionConvertida = await conexion.json();
+    return conexionConvertida;
+  } catch (error) {
+    alert("Error en la conexión: " + error.message);
+    console.error(error);
+  }
+}
+
+export const conexionAPI = {
+  listaProductos,
+  nuevoProducto,
+  eliminarTarjeta
+};
